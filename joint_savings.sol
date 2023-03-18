78cc090d832da0517d44ae13963fc09b1f44fab1
Audell <asabeti@outlook.com>
2023-03-18 00:57:45 -0700

    Called contractBalance variable

--- joint_savings.sol
+++ joint_savings.sol
@@ -53,17 +53,20 @@ contract JointSavings {
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
         */
-        // YOUR CODE HERE!
+         if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        // YOUR CODE HERE!
+        recipient.transfer(amount)
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        // YOUR CODE HERE!
+        lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        // YOUR CODE HERE!
+         contractBalance = address(this).balance;
     }
+    
 
     // Define a `public payable` function named `deposit`.
     function deposit() public payable {
